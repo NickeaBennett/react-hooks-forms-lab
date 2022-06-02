b6b1d16d42787131233d6c8db9df0645a3783299
NickeaBennett <nickeabennett@gmail.com>
2022-06-02 14:40:58 -0400

    Assignment

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, onSearchChange, search }) {
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
-      <select name="filter" onChange={onCategoryChange}>
+      <input value={search} onChange={onSearchChange} type="text" name="search" placeholder="Search..." />
+      <select name="filter" onChange={onCategoryChange} >
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,9 +5,14 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [userInput, setUserInput] = useState("");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
+  }
+
+  function handleInputChange(e) {
+    setUserInput(e.target.value);
   }
 
   const itemsToDisplay = items.filter((item) => {
@@ -19,7 +24,7 @@ function ShoppingList({ items }) {
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleInputChange} search={userInput} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
